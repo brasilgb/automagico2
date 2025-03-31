95cdcaa5c7f350e2242f6012f0d4fa997942b8a7
Anderson Brasil <andersonbrasil72@gmail.com>
2025-03-31 17:11:18 -0300

    Push

---
+++ app/Filament/Pages/Dashboard.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Filament\Pages;
+
+use Filament\Pages\Page;
+
+class Dashboard extends Page
+{
+    protected static ?string $navigationIcon = 'heroicon-o-squares-2x2';
+
+    protected static ?string $title = 'Dashboard';
+
+    protected static ?string $navigationLabel = 'Dashboard';
+
+}
